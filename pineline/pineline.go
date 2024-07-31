0a963a46ae0833a8504be104f9e4ff9ee092fc1e
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-07-31 14:24:05 +0700

    refactor: make new pineline structure with stages

--- pineline/pineline.go
+++ pineline/pineline.go
@@ -1,50 +1,20 @@
 package pineline
 
-import "context"
+import (
+	"context"
+)
 
-type Context context.Context
+type Stage func(ctx context.Context) error
 
-type Handler func(ctx *Context) error
+func Pineline(stages ...Stage) Stage {
+	return func(ctx context.Context) error {
+		for _, stage := range stages {
+			err := stage(ctx)
+			if err != nil {
+				return err
+			}
+		}
 
-type Node struct {
-	handler Handler
-	next    *Node
-}
-
-func (n *Node) Exec(ctx *Context) error {
-	if n == nil || n.handler == nil {
 		return nil
 	}
-
-	if err := n.handler(ctx); err != nil {
-		return err
-	}
-
-	return n.next.Exec(ctx)
-}
-
-type Pineline struct {
-	ctx       *Context
-	firstNode *Node
-}
-
-func (p *Pineline) Exec() error {
-	if err := p.firstNode.Exec(p.ctx); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func NewFirstNode(handlers ...Handler) *Node {
-	firstNode := &Node{}
-	curNode := firstNode
-
-	for _, handler := range handlers {
-		curNode.handler = handler
-		curNode.next = &Node{}
-		curNode = curNode.next
-	}
-
-	return firstNode
 }
