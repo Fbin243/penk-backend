8f0ee41cdc78a6c17b750baeee00aae027382f18
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-10-01 13:32:43 +0700

    chore: setup cli

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "tenk-cli",
+		Usage: "say hi to tenk-cli",
+		Action: func(*cli.Context) error {
+			fmt.Println("hieu dep trai sieu cap vu tru")
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("added task: ", cCtx.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
