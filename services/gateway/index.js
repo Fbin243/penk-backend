592c29611f996668c8e48ac90af9c56ad8be9f91
Nguyễn Thanh Bình <ntbinh243.dev@gmail.com>
2024-11-14 17:09:39 +0000

    chore: uncomment endpoints in gateway

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -5,9 +5,9 @@ const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       { name: "core", url: "http://localhost:8080/graphql" },
-      //   { name: "timetrackings", url: "http://localhost:8082/graphql" },
-      //   { name: "analytics", url: "http://localhost:8083/graphql" },
-      //   { name: "notification", url: "http://localhost:8084/graphql" },
+      { name: "timetrackings", url: "http://localhost:8082/graphql" },
+      { name: "analytics", url: "http://localhost:8083/graphql" },
+      { name: "notification", url: "http://localhost:8084/graphql" },
     ],
   }),
   buildService({ name, url }) {
