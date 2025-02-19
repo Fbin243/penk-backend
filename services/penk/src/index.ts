5e4944f5c509161b0f2983f9f20b8e2b065e0bd9
hieunguyen <dev.ngtronghieu@gmail.com>
2025-02-19 17:59:00 +0700

    refactor: setup graphql endpoint

--- services/penk/src/index.ts
+++ services/penk/src/index.ts
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+
+import { schema } from "./schema";
 
 dotenv.config({
   path: ".env.penk",
@@ -8,7 +11,9 @@ dotenv.config({
 const app = express();
 const port = 3011;
 
-app.get("/", (req, res) => {
+app.all("/graphql", createHandler({ schema }));
+
+app.get("/", (_, res) => {
   res.send("Hello PenK!");
 });
 
