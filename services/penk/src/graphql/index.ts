ef908f4daf50ecd84d84bdc974f1c2e42d267373
hieunguyen <dev.ngtronghieu@gmail.com>
2025-02-20 12:56:11 +0700

    chore: setup graphql codegen

--- services/penk/src/graphql/index.ts
+++ services/penk/src/graphql/index.ts
@@ -3,11 +3,13 @@ import { gql } from "apollo-server";
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
+import { Resolvers } from "./__generated__/resolvers-types";
+
 const typeDefs = gql(
   readFileSync(resolve(__dirname, "schema.graphql"), "utf8"),
 );
 
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
     helloPenK: () => "PenK",
   },
