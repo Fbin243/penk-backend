d8137e56853985f103244e4c454a0f7376823ca1
Nam <91814177+Namiscrea7or@users.noreply.github.com>
2025-03-26 20:19:16 +0700

    Fixed: handle store token in db
    * chore: compose to notiBiz
    
    * Fixed: create new data for new ID
    
    * chore: small change
    
    * chore: remove log

--- services/notification/business/interfaces.go
+++ services/notification/business/interfaces.go
@@ -10,12 +10,8 @@ import (
 type INotificationBusiness interface {
 	AddEmailToWaitlist(ctx context.Context, email string) error
 	SendPushNotification(ctx context.Context, notiReq *entity.SendNotiReq) (bool, error)
-}
-
-type IDeviceTokenBusiness interface {
 	RegisterDeviceToken(ctx context.Context, profileID, token, deviceID, platform string) (bool, error)
 	RemoveDeviceToken(ctx context.Context, profileID, token string) (bool, error)
-	GetDeviceTokenByDeviceID(ctx context.Context, deviceID string) (string, error)
 }
 
 // repository
