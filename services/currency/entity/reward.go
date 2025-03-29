a9d1e413ff75521fbdc1c550fe33693104fbdce9
Nam <91814177+Namiscrea7or@users.noreply.github.com>
2025-03-29 20:02:56 +0700

    Fixed: Enable ClaimedAt field nullable
    * Fixed: claimedAt nullable and handle time received logic
    
    * chore: remove log
    
    * chore: convert to asia hcm time

--- services/currency/entity/reward.go
+++ services/currency/entity/reward.go
@@ -9,7 +9,7 @@ type Reward struct {
 	*base.BaseEntity `                           bson:",inline"`
 	ProfileID        string    `json:"profileID,omitempty" bson:"profile_id,omitempty"`
 	FishCount        int32     `json:"fishCount"              bson:"fish_count"`
-	ClaimedAt        time.Time `json:"claimedAt,omitempty" bson:"claimed_at,omitempty"`
+	ClaimedAt        time.Time `json:"claimedAt,omitempty" bson:"claimed_at"`
 	StreakCount      int32     `json:"streakCount,omitempty" bson:"streak_count"`
 }
 
--- services/currency/repo/mongo/models.go
+++ services/currency/repo/mongo/models.go
@@ -11,7 +11,7 @@ type Reward struct {
 	*mongodb.BaseEntity `                           bson:",inline"`
 	ProfileOID          primitive.ObjectID `json:"profileID,omitempty" bson:"profile_id,omitempty"`
 	FishCount           int32              `json:"fishCount"              bson:"fish_count"`
-	ClaimedAt           time.Time          `json:"claimedAt,omitempty" bson:"claimed_at,omitempty"`
+	ClaimedAt           time.Time          `json:"claimedAt,omitempty" bson:"claimed_at"`
 	StreakCount         int32              `json:"streakCount,omitempty" bson:"streak_count"`
 }
 
