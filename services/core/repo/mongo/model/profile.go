db8c6941d61a177c9e4466a0630c0375485e4b63
Fbin243 <ntbinh243.dev@gmail.com>
2025-04-09 18:53:42 +0700

    fix: delete and recreate new profile

--- services/core/repo/mongo/model/profile.go
+++ services/core/repo/mongo/model/profile.go
@@ -3,21 +3,18 @@ package mongomodel
 import (
 	mongodb "tenkhours/pkg/db/mongo"
 
-	"github.com/samber/lo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Profile struct {
 	*mongodb.BaseEntity `                                    bson:",inline"`
-	Name                string              `json:"name,omitempty"               bson:"name"`
-	Email               string              `json:"email,omitempty"              bson:"email"`
-	FirebaseUID         string              `json:"firebaseUID,omitempty"        bson:"firebase_uid"`
-	ImageURL            string              `json:"imageURL,omitempty"           bson:"image_url"`
-	CurrentCharacterOID *primitive.ObjectID `json:"currentCharacterID,omitempty" bson:"current_character_id"`
+	Name                string             `json:"name,omitempty"               bson:"name"`
+	Email               string             `json:"email,omitempty"              bson:"email"`
+	FirebaseUID         string             `json:"firebaseUID,omitempty"        bson:"firebase_uid"`
+	ImageURL            string             `json:"imageURL,omitempty"           bson:"image_url"`
+	CurrentCharacterOID primitive.ObjectID `json:"currentCharacterID,omitempty" bson:"current_character_id"`
 }
 
-func (p *Profile) CurrentCharacterID(id *string) {
-	if id != nil {
-		p.CurrentCharacterOID = lo.ToPtr(mongodb.ToObjectID(*id))
-	}
+func (p *Profile) CurrentCharacterID(id string) {
+	p.CurrentCharacterOID = mongodb.ToObjectID(id)
 }
