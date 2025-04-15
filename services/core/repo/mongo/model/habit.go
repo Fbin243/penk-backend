b2e972fc7d3251fa7fde28cbdc8a3cbaa4fcbfb0
Fbin243 <ntbinh243.dev@gmail.com>
2025-04-15 19:05:00 +0700

    feat: add reset duration

--- services/core/repo/mongo/model/habit.go
+++ services/core/repo/mongo/model/habit.go
@@ -17,6 +17,7 @@ type Habit struct {
 	Value               float64               `json:"value,omitempty" bson:"value"`
 	Unit                *string               `json:"unit,omitempty"  bson:"unit"`
 	RRule               string                `json:"rrule"           bson:"rrule"`
+	Reset               entity.HabitReset     `json:"reset"           bson:"reset"`
 }
 
 func (h *Habit) CharacterID(id string) {
