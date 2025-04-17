29cd35e68b2f0640e08077ea051550befd5f68df
hieunguyen <dev.ngtronghieu@gmail.com>
2025-04-17 18:26:29 +0700

    chore: use env var for redirect uri

--- services/penk/src/utils/googleapis/config.ts
+++ services/penk/src/utils/googleapis/config.ts
@@ -3,5 +3,5 @@ import { google } from "googleapis";
 export const oauth2Client = new google.auth.OAuth2(
   `${process.env.GOOGLE_OAUTH_APP_GUID}.apps.googleusercontent.com`,
   process.env.WEB_CLIENT_SECRET,
-  `http://localhost:8097/oauth_redirect`,
+  process.env.GOOGLE_OAUTH_REDIRECT_URI,
 );
